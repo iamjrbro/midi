86677655536696d028e50db7634157a6deefd963
Julia Ribeiro <jrstudin@gmail.com>
2023-08-21 19:22:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
-document.querySelector('.tecla_pom')
+function playSoundPom() {
+  document.querySelector("#sound_key_pom").play();
+}
+document.querySelector(".key_pom").onclick = playSoundPom;
 
-document.querySelector('.tecla_clap')
+function playSoundClap() {
+  document.querySelector("#sound_key_clap").play();
+}
+document.querySelector(".key_clap").onclick = playSoundClap;
 
-document.querySelector('.tecla_tim')
+function playSoundTim() {
+  document.querySelector("#sound_key_tim").play();
+}
+document.querySelector(".key_tim").onclick = playSoundTim;
 
-document.querySelector('.tecla_puff')
+function playSoundPuff() {
+  document.querySelector("#sound_key_puff").play();
+}
+document.querySelector(".key_puff").onclick = playSoundPuff;
 
-document.querySelector('.tecla_splash')
+function playSoundSplash() {
+  document.querySelector("#sound_key_splash").play();
+}
+document.querySelector(".key_splash").onclick = playSoundSplash;
 
-document.querySelector('.tecla_toim')
+function playSoundToim() {
+  document.querySelector("#sound_key_toim").play();
+}
+document.querySelector(".key_toim").onclick = playSoundToim;
 
-document.querySelector('.tecla_psh')
+function playSoundPsh() {
+  document.querySelector("#sound_key_splash").play();
+}
+document.querySelector(".key_psh").onclick = playSoundPsh;
 
-document.querySelector('.tecla_tic')
+function playSoundTic() {
+  document.querySelector("#sound_key_tic").play();
+}
+document.querySelector(".key_tic").onclick = playSoundTic;
 
-document.querySelector('.tecla_tom')
+function playSoundTom() {
+  document.querySelector("#sound_key_tom").play();
+}
+document.querySelector(".key_tom").onclick = playSoundTom;
