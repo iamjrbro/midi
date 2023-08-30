65795d7e53e6219e1b06ad4273d0e9557a025af1
Julia Ribeiro <jrstudin@gmail.com>
2023-08-30 12:45:24 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,28 @@
-const keyList = document.querySelectorAll('.key');
-
-
-//functions
-
-function playSoundPom(){
-  document.querySelector('#sound_key_pom').play();
+function playSound(idAudioElement) {
+  const element = document.querySelector(idAudioElement);
+  if(element && element.localName === 'audio'){
+    element.play();
+  } else{
+    console.log('Element not found ou selector is invalid')
+  }
 }
-
-function playSoundClap(){
-  document.querySelector('#sound_key_clap').play();
-}
-
-function playSoundTim(){
-  document.querySelector('#sound_key_tim').play();
-}
-
-function playSoundPuff(){
-  document.querySelector('#sound_key_puff').play();
-}
-
-function playSoundSplash(){
-  document.querySelector('#sound_key_splash').play();
-}
-
-function playSoundToim(){
-  document.querySelector('#sound_key_toim').play();
-}
-
-function playSoundPsh(){
-  document.querySelector('#sound_key_psh').play();
-}
-
-function playSoundTic(){
-  document.querySelector('#sound_key_tic').play();
-}
-
-function playSoundTom(){
-  document.querySelector('#sound_key_tom').play();
-}
-
 
 //REPRODUCTON LIST
-keysList[0].onclick = playSoundPom;
-keyList[1].onclick = playSoundClap;
-keyList[2].onclick = playSoundTim;
-keyList[3].onclick = playSoundPuff;
-keyList[4].onclick = playSoundToim;
-keyList[5].onclick = playSoundPsh;
-keyList[6].onclick = playSoundTic;
-keyList[7].onclick = playSoundTom;
 
+const keyList = document.querySelectorAll('.key');
 
+for (let counter = 0; counter<keyList.length; counter++){
+  const key = keyList[counter];
+  const instrument = key.classList[1];
+  const idAudio = `#som_${instrument}`;
+
+  key.onclick = function(){
+    playSound(idAudio);
+  }
+  key.onkeydown = function(event){
+    key.classList.add('active');
+  }
+  key.onkeyup = function(){
+    key.classList.remove('active')
+  }
+}
